9139bdbb08379cccfbef9232983256df0f26b845
mmitch2958 <mikescottmitchell@outlook.com>
2020-08-07 12:02:28 -0400

    finsihed Cards construction

--- components/Cards.js
+++ components/Cards.js
@@ -20,3 +20,63 @@
 // Add a listener for click events so that when a user clicks on a card, the headline of the article is logged to the console.
 //
 // Use your function to create a card for each of the articles, and append each card to the DOM.
+function cardCreator({ headline, authorPhoto, authorName }) {
+   //Construct the components 
+    const cardWrapper = document.createElement('div')
+    const name = document.createElement('span')
+    const heading = document.createElement('div')
+    const author = document.createElement('div')
+    const imageContainer = document.createElement('div')
+    const image = document.createElement('img')
+    //Construct the data source 
+    cardWrapper.className = 'card'
+    author.className = 'author'
+    imageContainer.className = 'img-container'
+    name.textContent = `By ${authorName}`
+    heading.textContent = `${headline}`
+    heading.classList.add('headline')
+    image.src = authorPhoto
+   
+    
+    //append the data 
+    cardWrapper.appendChild(heading)
+    cardWrapper.appendChild(author)
+    author.appendChild(imageContainer)
+    imageContainer.appendChild(image)
+    author.appendChild(name)
+
+  console.log(cardWrapper)
+  return cardWrapper
+}
+
+const articlesURL = 'https://lambda-times-api.herokuapp.com/articles'
+
+axios.get(articlesURL)
+  .then((value) => {
+    const articles = value.data.articles
+    const cardContainer = document.querySelector('.cards-container')
+
+    for (let topics in articles) {
+      articles[topics].forEach((item) => {
+        cardContainer.appendChild(cardCreator(item))
+      })
+    }
+
+    function createCard(type) {
+      type.forEach((item) => {
+        cardContainer.appendChild(cardCreator(item))
+      })
+    }
+
+    const card = document.querySelectorAll('.card')
+    card.forEach((card) => {
+      const heading = card.querySelector('.headline')
+      card.addEventListener('click', () => {
+        console.log(heading.textContent)
+      })
+    })
+
+  })
+  .catch((elm) => {
+    console.log(elm)
+  })
